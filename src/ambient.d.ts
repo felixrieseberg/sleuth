d10af5d3bdbd74ec37c291bdeebedf1acbfbc5d7
Felix Rieseberg <felix@felixrieseberg.com>
2017-02-22 11:09:05 -0800

    :package: Include dependencies

--- src/ambient.d.ts
+++ src/ambient.d.ts
@@ -1,7 +1,9 @@
 //ambient type declarations for module does not have type definitions
 declare module 'url';
 declare module 'yauzl';
-declare module 'es6-promisify'
+declare module 'es6-promisify';
+declare module 'react-json-tree';
+declare module 'jsonic';
 
 //https://github.com/Microsoft/TypeScript/wiki/What's-new-in-TypeScript#wildcard-character-in-module-name
 declare module '*/package.json';
