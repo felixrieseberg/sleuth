f3ce965f85bc06af4d5bc7d8bb9211a0740ef523
Felix Rieseberg <felix@felixrieseberg.com>
2017-02-24 14:38:22 -0800

    :mag: Suggest from downloads folder

--- src/renderer/components/welcome.tsx
+++ src/renderer/components/welcome.tsx
@@ -1,18 +1,26 @@
 import * as React from 'react';
-import  * as os from 'os';
+import * as os from 'os';
+import * as fs from 'fs-promise';
+import * as path from 'path';
+
+import { remote } from 'electron';
 
 export interface WelcomeState {
   sleuth: string;
+  suggestions: Array<string>;
 }
 
-
-export class Welcome extends React.Component<undefined, WelcomeState> {
+export class Welcome extends React.Component<undefined, Partial<WelcomeState>> {
   constructor() {
     super();
 
     this.state = {
       sleuth: this.getSleuth()
     };
+  }
+
+  componentDidMount() {
+    this.getItemsInDownloadFolder();
   }
 
   public getSleuth() {
@@ -25,12 +33,48 @@ export class Welcome extends React.Component<undefined, WelcomeState> {
     }
   }
 
+  public getItemsInDownloadFolder(): void {
+    const dir = remote.app.getPath('downloads');
+
+    fs.readdir(dir)
+      .then((contents) => {
+        let suggestions: Array<string> = [];
+
+        contents.forEach((file) => {
+          if (file.startsWith('logs')) {
+            suggestions.push(path.join(dir, file));
+          }
+        });
+
+        this.setState({ suggestions });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   public render() {
+    const { suggestions, sleuth } = this.state;
+    let suggestion = null;
+
+    if (suggestions && suggestions.length > 0) {
+      suggestion = (
+        <div className='Suggestions'>
+          <h5>From your Downloads folder, may we suggest:</h5>
+          {suggestions.map((file) => <a className="small" href={file}>{path.basename(file)}</a> )}
+        </div>
+      );
+    }
+
     return (
       <div className='Welcome'>
-          <h1 className='Emoji'>{this.state.sleuth}</h1>
+        <i />
+        <div>
+          <h1 className='Emoji'>{sleuth}</h1>
           <h2>Hey there!</h2>
           <h4>Just drop a logs file or folder here.</h4>
+        </div>
+        {suggestion}
       </div>
     );
   }
