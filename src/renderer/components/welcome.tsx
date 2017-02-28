d3e0aae8a275c7c9e0c38811493189ecf16da524
Felix Rieseberg <felix@felixrieseberg.com>
2017-02-28 10:27:57 -0800

    :art: Limit emoji on Windows

--- src/renderer/components/welcome.tsx
+++ src/renderer/components/welcome.tsx
@@ -24,10 +24,13 @@ export class Welcome extends React.Component<undefined, Partial<WelcomeState>> {
   }
 
   public getSleuth() {
-    const sleuths = ['🕵', '🕵️‍♀️', '🕵🏻', '🕵🏼', '🕵🏽', '🕵🏾', '🕵🏿', '🕵🏻‍♀️', '🕵🏼‍♀️', '🕵🏽‍♀️', '🕵🏾‍♀️', '🕵🏿‍♀️'];
+    let sleuths = ['🕵', '🕵️‍♀️', '🕵🏻', '🕵🏼', '🕵🏽', '🕵🏾', '🕵🏿', '🕵🏻‍♀️', '🕵🏼‍♀️', '🕵🏽‍♀️', '🕵🏾‍♀️', '🕵🏿‍♀️'];
 
-    if (process.platform === 'darwin' || (process.platform === 'win32' && os.release().startsWith('10'))) {
+    if (process.platform === 'darwin') {
       return sleuths[Math.floor(Math.random() * 11) + 1];
+    } else if (process.platform === 'win32' && os.release().startsWith('10')) {
+      sleuths = ['🕵', '🕵🏻', '🕵🏼', '🕵🏽', '🕵🏾', '🕵🏿'];
+      return sleuths[Math.floor(Math.random() * 5) + 1];
     } else {
       return sleuths[Math.round(Math.random())];
     }
