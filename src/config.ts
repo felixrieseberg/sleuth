4a651e2d27510db348ec30d8d901ff2e240bccef
Felix Rieseberg <felix@felixrieseberg.com>
2017-07-11 16:15:28 -0700

    :wrench: Use Heroku server

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  cooperUrl: 'https://desktop-build-bot.now.sh',
+  cooperUrl: 'https://desktop-build-bot.herokuapp.com',
   teamName: 'tinyspeck',
   isDevMode: process.execPath.match(/[\\/]electron/)
 };
