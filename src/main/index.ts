eee4f4e8f6e9299d4dd8dd844bf186a8e034b9e3
Felix Rieseberg <felix@felixrieseberg.com>
2018-03-21 17:02:38 -0700

    :art: Fix Electron 2 API for hiddenInset

--- src/main/index.ts
+++ src/main/index.ts
@@ -30,7 +30,7 @@ if (require('electron-squirrel-startup')) {
       show: !!config.isDevMode,
       minHeight: 500,
       minWidth: 1000,
-      titleBarStyle: process.platform === 'darwin' ? 'hidden-inset' : undefined
+      titleBarStyle: process.platform === 'darwin' ? 'hiddenInset' : undefined
     });
 
     mainWindowState.manage(mainWindow);
