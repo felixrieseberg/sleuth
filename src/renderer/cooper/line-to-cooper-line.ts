79fffc83d8f40aa0475d4c02a863a9dd5ce34751
Felix Rieseberg <felix@felixrieseberg.com>
2019-03-28 17:24:42 -0700

    feat: Add more line filters

--- src/renderer/cooper/line-to-cooper-line.ts
+++ src/renderer/cooper/line-to-cooper-line.ts
@@ -17,55 +17,82 @@ export interface ExtraReplacer {
 
 const filters: Array<Replacer> = [
   {
+    name: 'team-ids',
+    rgx: /((?:\(| |^|')*(?:"{0,1}T[A-Z0-9]{8}"{0,1})(?:\)|,| |'|$)){2,}/g,
+    replacer: ` {Team IDs} `
+  }, {
+    name: 'user-ids',
+    rgx: /((?:\(| |^|')*(?:"{0,1}(?:U|W|B)[A-Z0-9]{8}"{0,1})(?:\)|,| |'|$)){2,}/g,
+    replacer: ` {User IDs} `
+  }, {
+    name: 'channel-ids',
+    rgx: /((?:\(| |^|')*(?:"{0,1}C[A-Z0-9]{8}"{0,1})(?:\)|,| |'|$)){2,}/g,
+    replacer: ` {Channel IDs} `
+  }, {
     name: 'team-id',
-    rgx: /( |^|')("{0,1}T[A-Z0-9]{8}"{0,1})( |'|$)/g,
+    rgx: /(\(| |^|')("{0,1}T[A-Z0-9]{8}"{0,1})(\)| |'|$)/g,
     replacer: ' {Team ID} '
-  },
-  {
+  }, {
     name: 'user-id',
-    rgx: /( |^)("{0,1}U[A-Z0-9]{8}"{0,1})( |$)/g,
+    rgx: /( |^)("{0,1}(?:U|W|B)[A-Z0-9]{8}"{0,1})( |$)/g,
     replacer: ' {User} '
-  },
-  {
+  }, {
     name: 'ms',
     rgx: /( |^)(\d{1,20}(?: )?ms)( |$)/g,
-    replacer: ' {time period} '
-  },
-  {
+    replacer: ' {Time period} '
+  }, {
+    name: 'old-timestamp',
+    rgx: /\d{4}\/\d{1,2}\/\d{1,2} \d{1,2}:\d{1,2}:\d{1,2}.\d{3}/g,
+    replacer: ` {Timestamp} `
+  }, {
     name: 'number',
-    rgx: /( |^)(\d*,?\d*)( |$)/g,
+    rgx: /( |^)(\d+(,|\.)?\d*)( |$)/g,
     replacer: ' {number} '
-  },
-  {
+  }, {
     name: 'id',
     rgx: /( |^)id (\d{1,5})( |$)/g,
     replacer: ' {id} '
-  },
-  {
+  }, {
     name: 'channel-id',
     rgx: /( |^)("{0,1}(C|D)[A-Z0-9]{8}"{0,1})( |$)/g,
     replacer: ' {Channel} '
-  },
-  {
+  }, {
     name: 'was-active',
     rgx: /(was active for )\d{1,12}/g,
     replacer: '$1{Time}'
-  },
-  {
+  }, {
     name: 'usage',
     rgx: /({Team ID} usage: )\d{0,20}/g,
     replacer: '$1{Time}'
-  },
-  {
+  }, {
     name: 'deep-link',
     rgx: /slack:\/\/channel\?team=(T[A-Z0-9]{8})&id=(C|D)[A-Z0-9]{8}/,
     replacer: 'slack://channel?team={Team ID}&id={Channel ID}'
-  },
-  {
+  }, {
     name: 'double-space',
     rgx: /  /g,
     replacer: ' '
-  },
+  }, {
+    name: 'emoji',
+    rgx: /(?: |^):[a-z-_]{1,}:(?: |$)/g,
+    replacer: ' {Emoji} '
+  }, {
+    name: 'shipit-line',
+    rgx: /^ShipIt\[\d+:\d+\] /g,
+    replacer: ' {ShipIt Line} '
+  }, {
+    name: 'webapp-hash',
+    rgx: /( +|^)(?:[a-z0-9]{8}-\d{10}\.\d{3})(?: +|$)/g,
+    replacer: ' {Hash} '
+  }, {
+    name: 'file-url',
+    rgx: /(?:^| |")file:\/\/[\S]+(?:$| |")/g,
+    replacer: ` {File Path} `
+  }, {
+    name: 'file-url',
+    rgx: /(?:^| |")https?:\/\/[\S]+(?:$| |")/g,
+    replacer: ` {HTTP URL} `
+  }
 ];
 
 export class LineToCooperLine {
@@ -109,6 +136,8 @@ export class LineToCooperLine {
     filters.forEach(({ rgx, replacer }) => {
       output = output.replace(rgx, replacer);
     });
+
+    filters.forEach(({ rgx }) => (rgx.lastIndex = 0));
 
     return output.trim();
   }
