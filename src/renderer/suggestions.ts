695702c67d2723a7d7b2212f96db9efaa9f1316a
Felix Rieseberg <felix@felixrieseberg.com>
2020-02-03 14:41:27 -0800

    feat: Add #alertds-desktop-logs to suggestions

--- src/renderer/suggestions.ts
+++ src/renderer/suggestions.ts
@@ -15,7 +15,15 @@ export async function getItemsInDownloadFolder(): Promise<Suggestions> {
     const contents = await fs.readdir(dir);
 
     for (const file of contents) {
-      if (file.startsWith('logs') || file.startsWith('slack-logs')) {
+      // If the file is from #alerts-desktop-logs, the server will
+      // have named it, not the desktop app itself.
+      // It'll look like T8KJ1FXTL_U8KCVGGLR_1580765146766674.zip
+      const serverFormat = /\w{8}_\w{8,9}_\d{16}.zip/
+      const shouldAdd = file.startsWith('logs') ||
+        file.startsWith('slack-logs') ||
+        serverFormat.test(file);
+
+      if (shouldAdd) {
         const filePath = path.join(dir, file);
         const stats = fs.statSync(filePath);
         const age = formatDistanceToNow(stats.mtimeMs);
