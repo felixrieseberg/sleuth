6cfb3cf7e3c07628ec22904fc68d46767a40cd08
Felix Rieseberg <felix@felixrieseberg.com>
2020-02-23 17:47:01 -0800

    chore: Add some padding

--- src/renderer/components/cachetool.tsx
+++ src/renderer/components/cachetool.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { observer } from 'mobx-react';
-import { Card } from '@blueprintjs/core';
+import { Card, Callout, Intent } from '@blueprintjs/core';
 
 import { SleuthState } from '../state/sleuth';
 
@@ -16,13 +16,39 @@ export interface CacheToolProps {
 @observer
 export class CacheTool extends React.Component<CacheToolProps, Partial<CacheToolState>> {
   public render() {
+    const warning = this.renderPlatformWarning();
+    const content = warning ? warning : this.renderTool();
+
     return (
-      <>
-        <Card className='StateTable-Info'>
-          <h5>Cachetool</h5>
-          <p>This tool allows you to closely examine Slack's cache.</p>
-        </Card>
-      </>
+      <div className='cachetool'>
+        {content}
+      </div>
     );
+  }
+
+  private renderTool() {
+    return (
+      <Card className='StateTable-Info'>
+        <h5>Cachetool</h5>
+        <p>This tool allows you to closely examine Slack's cache.</p>
+      </Card>
+    );
+  }
+
+  private renderPlatformWarning(): JSX.Element | null {
+    if (process.platform !== 'darwin') {
+      return (
+        <Callout intent={Intent.WARNING}>
+          <p>
+            Bad news: This tool is currently only supported on macOS.
+            It can analze caches from Slack, Electron, or Chrome ran
+            on any platform, but the tool itself can only do so when
+            running on macOS.
+          </p>
+        </Callout>
+      );
+    } else {
+      return null;
+    }
   }
 }
