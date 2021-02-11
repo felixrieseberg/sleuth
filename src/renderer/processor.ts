852bca325d13aca211832e5140b141d2234a4547
nocturnll <msbelanger93@gmail.com>
2021-02-10 17:27:39 -0800

    Split up Android and iOS line match functions
    * As the summary says

--- src/renderer/processor.ts
+++ src/renderer/processor.ts
@@ -693,16 +693,15 @@ export function matchLineConsole(line: string): MatchResult | undefined {
 
 
 /**
- * Matches a mobile (iOS or Android) line
+ * Matches an iOS line
  *
  * @param line
  * @returns {(MatchResult | undefined)}
  */
-export function matchLineMobile(line: string): MatchResult | undefined {
+export function matchLineIOS(line: string): MatchResult | undefined {
 
   if (line.startsWith('=====')) { return; } // We're ignoring these lines
 
-  // Let's see if it's an iOS log first
   IOS_RGX.lastIndex = 0;
   let results = IOS_RGX.exec(line);
 
@@ -728,13 +727,22 @@ export function matchLineMobile(line: string): MatchResult | undefined {
       momentValue
     };
   }
+  return;
+}
 
-  // Not iOS? Try Android
+
+/**
+ * Matches an Android line
+ *
+ * @param line
+ * @returns {(MatchResult | undefined)}
+ */
+export function matchLineAndroid(line: string): MatchResult | undefined {
+
   ANDROID_RGX.lastIndex = 0;
-  results = ANDROID_RGX.exec(line);
+  let results = ANDROID_RGX.exec(line);
 
   if (results && results.length === 3) {
-
     // Android timestamps have no year, so we gotta add one
     const currentDate = new Date();
     const newTimestamp = new Date(results[1]);
@@ -764,7 +772,6 @@ export function matchLineMobile(line: string): MatchResult | undefined {
       momentValue: new Date().valueOf(),
     };
   }
-
   return;
 }
 
@@ -822,7 +829,11 @@ export function getMatchFunction(
       return matchLineShipItMac;
     }
   } else if (logType === LogType.MOBILE) {
-    return matchLineMobile;
+    if (logFile.fileName.startsWith('attachment')) {
+      return matchLineAndroid;
+    } else {
+      return matchLineIOS;
+    }
   } else {
     return matchLineElectron;
   }
